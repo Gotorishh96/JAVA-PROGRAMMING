b6f1a1f831a093fe1fd2e496d528045aa2b8e9c9
Gotorishh96 <23cse101@abit.edu.in>
2024-12-01 13:45:13 +0530

    Add files via upload

---
+++ BankAccount.java
@@ -0,0 +1,27 @@
+
+package Account_Pack;
+
+   public class BankAccount {
+     String accountHolderName;
+     int accountNumber;
+     double balance;
+
+   public BankAccount(String name, int number, double initialBalance) {
+        accountHolderName = name;
+        accountNumber = number;
+        balance = initialBalance;
+    }
+
+    public void showAccountInfo() {
+        System.out.println("Account Holder Name: " + accountHolderName);
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Balance: ₹" + balance);
+    }
+  public static void main(String[] args) {
+        // Create a BankAccount object
+        BankAccount account = new BankAccount("RISHIKANTA PANDA",979650678,180000.00);
+        
+        // Show account information
+        account.showAccountInfo();
+    }
+   }
