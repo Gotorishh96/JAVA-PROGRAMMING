569b1557e1b3fed3d831e32e83975102decf92c6
Rishikanta Panda <23cse101@abit.edu.in>
2025-01-25 16:45:40 +0530

    Update ExceptionNested.java

--- ExceptionNested.java
+++ ExceptionNested.java
@@ -1,6 +1,6 @@
   public class ExceptionNested {
 
-    public static void main(String[] args) {
+     public static void main(String[] args) {
         try {
             // Outer try block
             System.out.println("Outer try block starts...");
